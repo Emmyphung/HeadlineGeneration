ece289cfbdfedc34d612a98a2bb351cff8e20d3e
emmyphung <emmy.phungt@gmail.com>
2021-04-01 11:37:06 -0400

    Update data_processing.py

--- dataprep/data_processing.py
+++ dataprep/data_processing.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
 import collections
 import json
 import multiprocessing
@@ -12,9 +11,14 @@ from newspaper import Article
 import requests
 import re
 
+from tqdm import tqdm
+import nltk
+import string
+nltk.download('punkt')
+
 class Processor():
     """
-    This processor takes in json filepath (containing urls of articles) as input
+    This processor takes in json filepath as input
     and return a dataframe with url, title, main tex of articles.  
     """
     def _get_url_from_json(self, file_path):
@@ -31,18 +35,29 @@ class Processor():
     
     def _get_content_from_url(self, url):
         """Takes in a single url and return article content and title"""
-        r = requests.get(url)
+        #r = requests.get(url)
+
+        try:
+            r = requests.get(url,timeout=4)
+            # print('successful!')
+        except requests.exceptions.Timeout as e:
+            # Maybe set up for a retry
+            print(e)
+            return ' ', ' '
+        except requests.exceptions.RequestException as e:
+            print(e)
+            return ' ', ' '
 
         # save to file
         with open('file.html', 'wb') as fh:
             fh.write(r.content)
-
+        #print('Running Article...')
         a = Article(url)
 
         # set html manually
         with open("file.html", 'rb') as fh:
             a.html = fh.read()
-
+        #print('Done opening Article.html...')
         # need to set download_state to 2 for this to work
         a.download_state = 2
 
@@ -52,22 +67,38 @@ class Processor():
         content = re.sub("\n\n"," ",a.text)
         # Now the article should be populated
         return content, title
+
+    def tokenize(self, text):
+        """ function: tokenize
+          ------------------
+          generate list of tokens given a block of @text;
+          :param text: string representing text field (title or body)
+          :returns: list of strings of tokenized & sanitized words
+        """
+        # remove punctuation
+        no_punctuation = text.translate(str.maketrans('','',string.punctuation))
+        # tokenize
+        tokens = nltk.word_tokenize(no_punctuation)
+        return tokens
     
     def process(self, filepath):
         """Main function of the Processor class"""
         print('Processing json and extracting urls...')
         urls = self._get_url_from_json(os.path.join(filepath))
         print('Done extracting urls - Begin extracting article content...')
+        print('There are {} urls...'.format(len(urls)))
         label = []
         text = []
-        for url in urls:
+        for url in tqdm(urls):
             content, title = self._get_content_from_url(url)
+            content, title = self.tokenize(content), self.tokenize(title)
             label.append(title)
             text.append(content)
+
         print('Done extracting article content...')
         df = {'url':urls, 'title':label, 'text':text}
         print('Outputing a dataframe...')
-        return pd.DataFrame(data=df)
+        return df
 
 # Example of command to use this class and methods:
 # df_test = Processor().process('folder_name/test.json')
